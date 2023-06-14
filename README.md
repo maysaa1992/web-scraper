23f283b600cdd057577c72461ac49a3b9d3327fe
maysaa1992 <batainehmaysaa@gmail.com>
2023-06-14 19:31:45 +0000

    lab17

---
+++ README.md
@@ -0,0 +1,6 @@
+# LAB - Class 17
+## Project: web_scraper
+**Author: ’Maysaa albataineh**
+
+## How to initialize/run your application (where applicable)
+python scraper.py
--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -2,65 +2,39 @@
 import requests
 from  bs4 import BeautifulSoup
 import json
-
- def get_citations_needed_count():
-     pass
- def  get_citations_needed_report():
-     pass
-
-
-
-URL = 'https://www.reddit.com/'
+URL = 'https://en.wikipedia.org/wiki/History_of_Mexico'
 page = requests.get(URL)
-# print(page.content)
-
 soup = BeautifulSoup(page.content,'html.parser')
-# print(soup)
-all_posts = soup.find_all('div', class_="_1poyrkZ7g36PawDueRza-J")
-# # print(all_posts)
-# all_cleaned_obj=[]
-# for post in all_posts:
-#     title = post.find('h3', class_="_eYtD2XCVieq6emjKBH3m").text.strip()
-#     # print(title)
-#     comments = post.find('span', class_="FHCV02u6Cp2zYL0fhQPsO").text
-#     # print(comments)
-
-#     likes = post.find('div', class_="_1rZYMD_4xY3gRcSS3p8ODO").text
-#     # print(likes)
-#     image_url = post.find('img')['src']
-#     # print(image_url)
 
 
-#     cleand_post={
-#         "title" :title,
-#         "comments" : comments,
-#         "likes" : likes,
-#         "image_url" : image_url
 
-#     }
+def get_citations_needed_count(url):
+    Num_Citation_needed = soup.find_all('sup', class_="noprint Inline-Template Template-Fact")    
 
-    # all_cleaned_obj.append(cleand_post)
+    return len(Num_Citation_needed)
+    
+def  get_citations_needed_report(URL):
+     paragraphs =soup.find_all('sup', class_="noprint Inline-Template Template-Fact") 
+    
+     for par in paragraphs:
+            pa=par.parent.text
+            print (par.parent.text)
+    
+ 
+ 
 
-### Refactoring 
-def clean_scraper(post):
-    title = post.find('h3', class_="_eYtD2XCVieq6emjKBH3m").text.strip()
-    comments = post.find('span', class_="FHCV02u6Cp2zYL0fhQPsO").text
-    likes = post.find('div', class_="_1rZYMD_4xY3gRcSS3p8ODO").text
-    image_url = post.find('img')['src']
+print(get_citations_needed_count(URL))
 
-    return {
-        "title" :title,
-        "comments" : comments,
-        "likes" : likes,
-        "image_url" : image_url
 
-    }
+get_citations_needed_report(URL)
+# for par in all_paragraphs:
+#     pa=par.parent.text
+#     print (par.parent.text)
+# print(needed_report)
 
-#list comprehension
-cleand_posts = [clean_scraper(post) for post in all_posts]
 
-# print(cleand_posts)
-
-data = json.dumps(cleand_posts)
-with open ('result.json','w') as file:
-    file.write(data)
+# data_count = json.dumps(len(needed_count))
+# dete_paragraphs=json.dumps(pa)
+# with open ('result.json','w') as file:
+#     file.write(data_count)
+#     file.write(pa)
